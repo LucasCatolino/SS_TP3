13b5944deccc6b9fb51eba5f615e734fdb36d0a8
Lucas <lcatolino@fi.uba.ar>
2022-04-11 11:52:25 -0300

    Minor refactor and add tests

--- src/core/Utils.java
+++ src/core/Utils.java
@@ -4,39 +4,41 @@ import java.awt.geom.Point2D;
 
 public class Utils {
 	
+	private static final int INFINITE= Integer.MAX_VALUE;
+	
 	public static double timeToCollision(Point2D p1, double[] v1, double r1, Point2D p2, double[] v2, double r2) {
 		
 		double dv_dr= deltaV_deltaR(p1.getX(), p2.getX(), v1[0], v2[0], p1.getY(), p2.getY(), v1[1], v2[1]);
 		if (dv_dr >= 0) {
-			return Integer.MAX_VALUE;
+			return INFINITE;
 		}
 		
 		double d= d(p1, v1, r1, p2, v2, r2);
 		if (d < 0) {
-			return Integer.MAX_VALUE;
+			return INFINITE;
 		}
 		
 		double dv_dv= deltaV_deltaV(v1[0], v2[0], v1[1], v2[1]);
 		return - ( dv_dr + Math.sqrt(d) ) / ( dv_dv );
 	}
 	
-	public static double sigma(double r1, double r2) {
+	private static double sigma(double r1, double r2) {
 		return r1 + r2;
 	}
 	
-	public static double deltaR_deltaR(double x1, double x2, double y1, double y2) {
+	private static double deltaR_deltaR(double x1, double x2, double y1, double y2) {
 		return Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
 	}
 	
-	public static double deltaV_deltaV(double vx1, double vx2, double vy1, double vy2) {
+	private static double deltaV_deltaV(double vx1, double vx2, double vy1, double vy2) {
 		return Math.pow(vx2 - vx1, 2) + Math.pow(vy2 - vy1, 2);
 	}
 	
-	public static double deltaV_deltaR(double x1, double x2, double vx1, double vx2, double y1, double y2, double vy1, double vy2) {
+	private static double deltaV_deltaR(double x1, double x2, double vx1, double vx2, double y1, double y2, double vy1, double vy2) {
 		return (vx2 - vx1)*(x2 - x1) + (vy2 - vy1)*(y2 - y1);
 	}
 	
-	public static double d(Point2D p1, double[] v1, double r1, Point2D p2, double[] v2, double r2) {
+	private static double d(Point2D p1, double[] v1, double r1, Point2D p2, double[] v2, double r2) {
 		double dv_dr= deltaV_deltaR(p1.getX(), p2.getX(), v1[0], v2[0], p1.getY(), p2.getY(), v1[1], v2[1]);
 		double dv_dv= deltaV_deltaV(v1[0], v2[0], v1[1], v2[1]);
 		double dr_dr= deltaR_deltaR(p1.getX(), p2.getX(), p1.getY(), p2.getY());
--- test/tests/Tests.java
+++ test/tests/Tests.java
@@ -9,9 +9,11 @@ import org.junit.Test;
 import core.Utils;
 
 public class Tests {
+	
+	private static final int INFINITE= Integer.MAX_VALUE;
 
 	@Test
-	public void test() {
+	public void test01ParticlesCollide() {
 		Point2D p1= new Point2D.Float();
 		p1.setLocation(0, 0);
 		double[] v1= {0, 0};
@@ -25,6 +27,40 @@ public class Tests {
 		double ttc= Utils.timeToCollision(p1, v1, r1, p2, v2, r2);
 
 		assertEquals(10, ttc, 0.1);
+	}
+	
+	@Test
+	public void test02ParticlesDoNotCollide() {
+		Point2D p1= new Point2D.Float();
+		p1.setLocation(0, 0);
+		double[] v1= {0, 0};
+		double r1= 0;
+		
+		Point2D p2= new Point2D.Float();
+		p2.setLocation(100, 0);
+		double[] v2= {10, 0};
+		double r2= 0;
+		
+		double ttc= Utils.timeToCollision(p1, v1, r1, p2, v2, r2);
+
+		assertEquals(INFINITE, ttc, 0.1);
+	}
+	
+	@Test
+	public void test03ParticlesDoNotCollide() {
+		Point2D p1= new Point2D.Float();
+		p1.setLocation(0, 0);
+		double[] v1= {-10, 0};
+		double r1= 0;
+		
+		Point2D p2= new Point2D.Float();
+		p2.setLocation(100, 0);
+		double[] v2= {10, 0};
+		double r2= 0;
+		
+		double ttc= Utils.timeToCollision(p1, v1, r1, p2, v2, r2);
+
+		assertEquals(INFINITE, ttc, 0.1);
 	}
 
 }
