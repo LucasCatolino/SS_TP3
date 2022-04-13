1e2cd412ee66fa7e3d22eae0795d9c28397e3e8f
Lucas <lcatolino@fi.uba.ar>
2022-04-13 20:24:13 -0300

    Finish wall returning point implementation and add tests for them

--- src/core/Wall.java
+++ src/core/Wall.java
@@ -15,40 +15,79 @@ public class Wall {
 
 	public Point2D getCollisionPoint(Particle part) {
 		Point2D collisionPoint= new Point2D.Float();
-		//Down wall and particle going up
-		if ('D' == id) {
+
+		//Left wall and particle going right
+		if ('L' == id) {
+			
+		}
+		double alpha= 0;
+		double x= part.getX();
+		double y= part.getY();
+		double auxX= 0;
+		double auxY= 0;
+		double vX= Math.abs(part.getVx());
+		double vY= Math.abs(part.getVy());
+		
+		switch (id) {
+		case 'D':
+			//particle going up
 			if (part.getVy() >= 0) {
 				collisionPoint.setLocation(INFINITE, INFINITE);
 				return collisionPoint;
 			}
-		}
-		//Right wall and particle going left
-		if ('R' == id) {
+			//particle going down
+			alpha= Math.atan2(vX, vY);
+			auxY= 0;
+			auxX= (part.getVx() > 0) ? y * Math.tan(alpha) + x : x - y * Math.tan(alpha);
+			break;
+			
+		case 'R':
+			//particle going left
 			if (part.getVx() <= 0) {
 				collisionPoint.setLocation(INFINITE, INFINITE);
 				return collisionPoint;
 			}
-		}
-		//Up wall and particle going down
-		if ('U' == id) {
+			//particle going right
+			alpha= Math.atan2(vY, vX);
+			auxX= limit;
+			auxY= (part.getVy() > 0) ? (limit - x) * Math.tan(alpha) + y : y - (limit - x) * Math.tan(alpha);
+			break;
+			
+		case 'U':
+			//particle going down
 			if (part.getVy() <= 0) {
 				collisionPoint.setLocation(INFINITE, INFINITE);
 				return collisionPoint;
 			}
-		}
-		//Left wall and particle going right
-		if ('L' == id) {
+			//particle going down
+			alpha= Math.atan2(vX, vY);
+			auxY= limit;
+			auxX= (part.getVx() > 0) ? (limit - y) * Math.tan(alpha) + x : x - (limit - y) * Math.tan(alpha);
+			break;
+			
+		case 'L':
+			//particle going right
 			if (part.getVx() >= 0) {
 				collisionPoint.setLocation(INFINITE, INFINITE);
 				return collisionPoint;
 			}
+			//particle going left
+			alpha= Math.atan2(vY, vX);
+			auxX= 0;
+			auxY= (part.getVy() > 0) ? x * Math.tan(alpha) + y : y - x * Math.tan(alpha);
+			break;
+
+		default:
+			auxX= INFINITE;
+			auxY= INFINITE;
+			break;
 		}
+		//check if particle reaches other wall before
+		auxX= (0 <= auxX && auxX <= limit) ? auxX : INFINITE;
+		auxY=  (0 <= auxY && auxY <= limit) ? auxY : INFINITE;
 
-		
-
-
-		// TODO Auto-generated method stub
-		return null;
+		collisionPoint.setLocation(auxX, auxY);
+		return collisionPoint;
 	}
 
 }
--- test/tests/Tests.java
+++ test/tests/Tests.java
@@ -66,14 +66,101 @@ public class Tests {
 	}
 	
 	@Test
-	public void test04WallDevuelvePuntoDeColision() {
-		Wall wallU= new Wall("U", 6);
-		Particle part= new Particle(1, 1, 0, 0, 0, 1, 0);
+	public void test04WallRDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("R", 6);
+		Particle part= new Particle(1, 1, 0, 1, 1, 1, 0);
 		
 		Point2D point= wallU.getCollisionPoint(part);
 		
+		assertEquals(6, point.getX(), 0.1);
+		assertEquals(1, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test05WallRDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("R", 6);
+		Particle part= new Particle(1, 1, 0, 1, 1, 5, 1);
 		
-		fail("Not implemented");
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(6, point.getX(), 0.1);
+		assertEquals(2, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test06WallRDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("R", 6);
+		Particle part= new Particle(1, 1, 0, 1, 1, 5, -1);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(6, point.getX(), 0.1);
+		assertEquals(0, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test06WallLDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("L", 6);
+		Particle part= new Particle(1, 1, 0, 3, 2, -3, -1);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(0, point.getX(), 0.1);
+		assertEquals(1, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test07WallLDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("L", 6);
+		Particle part= new Particle(1, 1, 0, 3, 2, -3, 1);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(0, point.getX(), 0.1);
+		assertEquals(3, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test08WallUDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("U", 6);
+		Particle part= new Particle(1, 1, 0, 3, 2, 1, 4);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(4, point.getX(), 0.1);
+		assertEquals(6, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test09WallUDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("U", 6);
+		Particle part= new Particle(1, 1, 0, 3, 2, -1, 4);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(2, point.getX(), 0.1);
+		assertEquals(6, point.getY(), 0.1);
+	}
+	
+	@Test
+	public void test10WallDDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("D", 6);
+		Particle part= new Particle(1, 1, 0, 3, 2, -1, -2);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(2, point.getX(), 0.1);
+		assertEquals(0, point.getY(), 0.1);
 	}
 
+	@Test
+	public void test11WallDDevuelvePuntoDeColision() {
+		Wall wallU= new Wall("D", 6);
+		Particle part= new Particle(1, 1, 0, 3, 2, 1, -2);
+		
+		Point2D point= wallU.getCollisionPoint(part);
+		
+		assertEquals(4, point.getX(), 0.1);
+		assertEquals(0, point.getY(), 0.1);
+	}
 }
